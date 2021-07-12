2f86f0ec91d0744b8fd2988e75ea68d99b81d418
GustavoAlessandro <filtroguga@gmail.com>
2021-07-12 15:34:12 -0300

    NodeJS

--- src/index.js
+++ src/index.js
@@ -20,8 +20,25 @@ function verifyUserExists(req, res, next){
     next();
 };
 
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(statement.type === 'deposit'){
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }}, 0);
+
+        return balance;
+};
+
 app.post("/account", (req, res) => {
     const { name, cpf } = req.body;
+
+    const customer = customers.some((customers) => customers.cpf === cpf);
+
+    if(customer){
+        return res.status(400).json({ Error: "Customer already exists!"});
+    }
 
     customers.push({
         name,
@@ -31,12 +48,68 @@ app.post("/account", (req, res) => {
     });
 
     return res.status(201).send();
-})
+});
 
 app.get("/account", verifyUserExists, (req, res) => {
     const { customer } = req;
 
     return res.json({customer});
+});
+
+app.get("/statement", verifyUserExists, (req, res) => {
+    const { customer } = req;
+
+    return res.send(customer.statement);
+});
+
+app.post("/statement/deposit", verifyUserExists, (req, res) => {
+    const { description, amount} = req.body;
+    const { customer } = req;
+
+    const statementOperation = ({
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'deposit'
+    });
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+});
+
+app.post("/statement/withdraw", verifyUserExists, (req, res) => {
+    const { amount } = req.body;
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+
+    if( balance < amount) {
+        return res.status(400).json({ Error: "Insufficient funds!"});
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'withdraw'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send();
+});
+
+app.put("/account", verifyUserExists, (req, res) => {
+    const { name } = req.body;
+    const { customer } = req;
+
+    customer.name = name;
+
+    return res.status(201).send();
+});
+
+app.delete("/account", verifyUserExists, (req, res) => {
+    //
 })
 
 app.listen(3333);
