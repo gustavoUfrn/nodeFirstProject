605fbf2a7f2fdacae8a6efc458e854a12058891c
GustavoAlessandro <filtroguga@gmail.com>
2021-07-12 13:59:26 -0300

    NodeJS

--- src/index.js
+++ src/index.js
@@ -4,19 +4,39 @@ const { v4 : uuidV4 } = require("uuid");
 
 app.use(express.json());
 
-const customer = [];
+const customers = [];
+
+function verifyUserExists(req, res, next){
+    const { cpf } = req.headers;
+
+    const customer = customers.find((customers) => customers.cpf === cpf);
+
+    if(!customer) {
+        return res.status(400).json({ Error: "Customer not found!"});
+    }
+
+    req.customer = customer;
+
+    next();
+};
 
 app.post("/account", (req, res) => {
     const { name, cpf } = req.body;
 
-    customer.push({
+    customers.push({
         name,
         cpf,
         id: uuidV4(),
         statement: []
     });
 
-    return res.send(customer);
+    return res.status(201).send();
+})
+
+app.get("/account", verifyUserExists, (req, res) => {
+    const { customer } = req;
+
+    return res.json({customer});
 })
 
 app.listen(3333);
